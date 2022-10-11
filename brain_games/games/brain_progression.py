b402b7e1ff11fff8798e863b7bc0dc2d2cac3047
stigsanek <alexander.e.egorov@pikta.ru>
2022-10-11 23:50:27 +0400

    Implements a brain-progression game

---
+++ brain_games/games/brain_progression.py
@@ -0,0 +1,80 @@
+import random
+
+from brain_games.games.base import get_random_num
+
+# Progression length
+PROGRESSION_LEN = 10
+
+# Hidden number symbol
+HIDDEN_EL = ".."
+
+
+def get_game_text() -> str:
+    """
+    Returns condition game text
+
+    :return: str
+    """
+    return "What number is missing in the progression?"
+
+
+def get_progression() -> str:
+    """
+    Returns progression
+
+    :return: str
+    """
+    start_num = get_random_num()
+    step = random.randint(1, PROGRESSION_LEN)
+    data = []
+
+    for i in range(0, PROGRESSION_LEN):
+        data.append(str(start_num))
+        start_num = start_num + step
+
+    data[step - 1] = HIDDEN_EL
+
+    return " ".join(data)
+
+
+def calculate_step(nums: list) -> int:
+    """
+    Calculate progression step
+
+    :param nums:
+    :return:
+    """
+    step = 0
+
+    for i in range(0, len(nums) - 1):
+        min_step = nums[i + 1] - nums[i]
+
+        if not step or step > min_step:
+            step = min_step
+
+    return step
+
+
+def get_answer(progression: str) -> str:
+    """
+    Returns correct answer
+
+    :param progression: nums progression
+    :return: str
+    """
+    data = progression.split(" ")
+    nums = []
+    idx = 0
+
+    for i in range(0, len(data)):
+        try:
+            nums.append(int(data[i]))
+        except ValueError:
+            idx = i
+
+    step = calculate_step(nums)
+
+    if idx == 0:
+        return str(nums[idx] - step)
+
+    return str(nums[idx - 1] + step)
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from brain_games.games.base import run_base_logic
+from brain_games.games.brain_progression import get_answer
+from brain_games.games.brain_progression import get_game_text, get_progression
+
+
+def main():
+    """
+    Main function
+
+    :return:
+    """
+    run_base_logic(
+        game_text=get_game_text(),
+        question_fn=get_progression,
+        answer_fn=get_answer
+    )
+
+
+if __name__ == "__main__":
+    main()
