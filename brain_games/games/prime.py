3ef7c3ccf1951e2ac60a878311c1cd242ac0a402
stigsanek <alexander.e.egorov@pikta.ru>
2022-10-12 00:07:23 +0400

    Implements a brain-prime game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,39 @@
+from brain_games.games.base import ANSWER_YES, ANSWER_NO
+
+
+def get_game_text() -> str:
+    """
+    Returns condition game text
+
+    :return: str
+    """
+    return f"Answer '{ANSWER_YES}' if given number is prime. " \
+           f"Otherwise answer '{ANSWER_NO}'."
+
+
+def is_prime_number(number: int) -> bool:
+    """
+    Checks if a number is prime
+
+    :param number: random number
+    :return: bool
+    """
+    if number > 1:
+        for i in range(2, int(number / 2) + 1):
+            if number % i == 0:
+                return False
+
+        else:
+            return True
+
+    return False
+
+
+def get_answer(number: int) -> str:
+    """
+    Returns correct answer
+
+    :param number: random number
+    :return: str
+    """
+    return ANSWER_YES if is_prime_number(number) else ANSWER_NO
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from brain_games.games.base import get_random_num, run_base_logic
+from brain_games.games.prime import get_game_text, get_answer
+
+
+def main():
+    """
+    Main function
+
+    :return:
+    """
+    run_base_logic(
+        game_text=get_game_text(),
+        question_fn=get_random_num,
+        answer_fn=get_answer
+    )
+
+
+if __name__ == "__main__":
+    main()
